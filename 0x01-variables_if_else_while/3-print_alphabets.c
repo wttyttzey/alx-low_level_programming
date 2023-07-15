9c9ee17a1c628fdf5ba8bd113f63382fe7e88169
wttyttzey <alekikimii@gmail.com>
2023-07-15 11:00:04 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always  0 (success)
+ * main - Entry point of the program
+ * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 	char c;
 	char d;
-	
+
 	c = 'a';
 	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(d <+ 'Z'){
-			putchar (d);
-			d++;
-		}
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+
 	putchar('\n');
 	return (0);
 }
+
