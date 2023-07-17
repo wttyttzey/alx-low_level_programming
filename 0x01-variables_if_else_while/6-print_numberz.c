6919b9a2c0231856f398db6c3bcb199d0bb7dcfc
wttyttzey <alekikimii@gmail.com>
2023-07-17 06:25:43 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-   int a;
-
-   a = 0;
-   while (a < 10)
-   {
-       putchar(a + '0');
-       a++;
-   }
-   putchar('\n');
-   return (0);
+int a;
+a = 0;
+while (a < 10)
+{
+putchar(a + '0');
+a++;
+}
+putchar('\n');
+return (0);
 }
