e7973a466078b66e45322b17bd9e421772769c9d
wttyttzey <alekikimii@gmail.com>
2023-07-17 06:39:52 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,24 +6,24 @@
  */
 int main(void)
 {
-    char d;
-    int h;
+char d;
+int h;
 
-    d = 'a';
-    h = 0;
+d = 'a';
+h = 0;
 
-    while (h < 10)
-    {
-         putchar(h + '0'); 
-         h++;
-    }
+while (h < 10)
+{
+putchar(h + '0'); 
+h++;
+}
 
-    while (d <= 'f')
-    { 
-         putchar(d); 
-         d++;
-    }
+while (d <= 'f')
+{ 
+putchar(d); 
+d++;
+}
 
-    putchar('\n');
-    return (0);
+putchar('\n');
+return (0);
 }
