f1da64ac4f769271fce388090e06878f5757f060
wttyttzey <alekikimii@gmail.com>
2023-07-17 06:18:55 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
 
-/** entry point
+/**
+ * main - Entry point
  *
- * main - print digits
- * return always 0 (success)
+ * Description: Prints all possible combinations of single-digit numbers
+ * separated by ", " in ascending order
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int a;
-
-for (a = 0; a < 10; a++)
+int i, j, k, l;
+for (i = 0; i <= 9; i++)
 {
-printf("%d", a);
-if (a < 9)
-printf(", ");
+putchar('0' + i); /* Print the first digit */
+
+for (j = i + 1; j <= 9; j++)
+{
+putchar(',');
+putchar(' ');
+putchar('0' + j); /* Print the second digit */
+
+for (k = j + 1; k <= 9; k++)
+{
+putchar(',');
+putchar(' ');
+putchar('0' + k); /* Print the third digit */
+
+for (l = k + 1; l <= 9; l++)
+{
+putchar(',');
+putchar(' ');
+putchar('0' + l); /* Print the fourth digit */
 }
-printf("\n");
+}
+}
+}
+
+putchar('\n'); /* Print a newline character at the end */
+
 return (0);
 }
