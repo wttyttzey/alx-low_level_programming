48fafa18883a8daa8f681f560a8868208a7b4515
wttyttzey <alekikimii@gmail.com>
2023-07-17 07:13:49 -0700

    9-print_comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,26 +5,25 @@
  */
 int main(void)
 {
-    int ones;
-    int tens;
+int ones;
+int tens;
 
-    for (tens = '0'; tens <= '9'; tens++)
-    {
-        for (ones = '0'; ones <= '9'; ones++)
-        {
-            if (!((ones == tens) || (tens > ones)))
-            {
-                putchar(tens);
-                putchar(ones);
-                if (!(ones == '9' && tens == '8'))
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return 0;
+for (tens = '0'; tens <= '9'; tens++)
+{
+for (ones = '0'; ones <= '9'; ones++)
+{
+if (!((ones == tens) || (tens > ones)))
+{
+putchar(tens);
+putchar(ones);
+if (!(ones == '9' && tens == '8'))
+{
+putchar(',');
+putchar(' ');
 }
-
+}
+}
+}
+putchar('\n');
+return 0;
+}
