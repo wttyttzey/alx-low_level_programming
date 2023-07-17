d3413f3ed7827e81adf58e2b14bee723cb1b1559
wttyttzey <alekikimii@gmail.com>
2023-07-17 06:03:53 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
+/** entry point
+ * main - print digits
+ * return always 0 (success)
+ */
 int main(void)
 {
-	int a;
+int a;
 
-	for (a = 0; a < 10; a++)
-	{
-		printf("%d", a);
-		if (a < 9)
-			printf(", ");
-	}
-	printf("\n");
-	return 0;
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+if (a < 9)
+printf(", ");
+}
+printf("\n");
+return (0);
 }
