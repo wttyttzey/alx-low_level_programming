ce8ea7a332207410ddfce9b8eeb049e67deb5732
wttyttzey <alekikimii@gmail.com>
2023-07-17 23:33:53 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ for (tens = '0'; tens <= '9'; tens++)
 {
 for (ones = '0'; ones <= '9'; ones++)
 {
-if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+if (!((ones == tens)||(tens == hundreds)||(tens > ones)||(hundreds > tens)))
 {
 putchar(hundreds);
 putchar(tens);
