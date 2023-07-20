14149403069dad26df62d75dadc8e06947a587b8
wttyttzey <alekikimii@gmail.com>
2023-07-19 22:14:00 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,9 +4,11 @@
  * main - Prints the sum of all multplies of 3 or 5 up to 1024
  * Return: always (success)
  */
+
 int main(void)
 {
 int i, z = 0;
+
 while (i < 1024)
 {
 if ((i % 3 == 0) || (i % 5 == 0))
