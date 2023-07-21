4437bf870f53ab0ba87354a4e4e8878cec30db88
wttyttzey <alekikimii@gmail.com>
2023-07-21 13:58:03 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,11 +17,11 @@ if ((n % 3 == 0) && (n % 5 == 0))
 {
 printf("FizzBuzz");
 }
-else if(n % 3 == 0)
+else if (n % 3 == 0)
 {
 printf("Fizz");
 }
-else if(n % 5 == 0)
+else if (n % 5 == 0)
 {
 printf("Buzz");
 }
