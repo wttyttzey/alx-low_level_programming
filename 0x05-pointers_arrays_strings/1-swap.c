428c6f81afc51836f5f81845d96a466645bc4628
wttyttzey <alekikimii@gmail.com>
2023-07-24 12:50:20 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,10 +7,10 @@
  * Return: nothing
  */
 
-void swap_int(int *a, int*b)
+void swap_int(int *a, int *b)
 /* the function that swaps the values of two integers. */
 {
-	
+
 int temp = *a;
 
 *a = *b;
