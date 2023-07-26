b063997be774914d9bf4e0114521904361bc95c8
wttyttzey <alekikimii@gmail.com>
2023-07-26 07:46:26 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check thr code 
+ *
+ * Return:
+ * Always:always 0.
+ */
+int main(void)
+
+{
+	print_number(98);
+	putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	return (0);
+}
+
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: 0 if the strings are equal, negative if s1 is less than s2,
+ *         and positive if s1 is greater than s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+
+while (s1[i] == s2[i])
+{
+if (s1[i] == '\0')
+return 0;
+i++;
+}
+
+return (s1[i] - s2[i]);
+}
+
