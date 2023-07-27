b6bf847d8fb956222d48c96015a9778231fb835b
wttyttzey <alekikimii@gmail.com>
2023-07-27 00:18:54 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,17 +6,17 @@
  * @s2: The second string.
  * Return: 0 if the strings are equal, negative if s1 is less
  */
-
 int _strcmp(char *s1, char *s2)
 {
 int i = 0;
 
 while (s1[i] == s2[i])
-{
+0{
 if (s1[i] == ('\0')
 return 0;
 i++;
 }
+
 return (s1[i] - s2[i]);
 }
 
