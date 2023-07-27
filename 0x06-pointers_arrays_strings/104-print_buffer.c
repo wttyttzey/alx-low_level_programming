61c2ab2e4dc879adced580c854c7f060cba0e275
wttyttzey <alekikimii@gmail.com>
2023-07-26 21:34:46 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,24 +1,45 @@
-#include "main.h"
-
+ #include "main.h"
 /**
- * main - check thr code 
- *
- * Return:
- * Always:always 0.
- */
-int main(void)
-
+* print_buffer - Prints the content of a buffer.
+* @b: The buffer to print.
+* @size: The number of bytes to print.
+*/
+void print_buffer(char *b, int size)
 {
-	print_number(98);
-	putchar('\n');
-	print_number(402);
-	_putchar('\n');
-	print_number(1024);
-	_putchar('\n');
-	print_number(0);
-	_putchar('\n');
-	print_number(-98);
-	_putchar('\n');
-	return (0);
+int i, j;
+
+if (size <= 0)
+
+putchar('\n');
+else
+{
+for (i = 0; i < size; i += 10)
+{
+printf("%08x: ", i);
+
+for (j = 0; j < 10; j++)
+{
+if (i + j < size)
+printf("%02x", b[i + j]);
+else
+printf("  ");
+
+if (j % 2)
+putchar(' ');
 }
 
+for (j = 0; j < 10; j++)
+{
+if (i + j < size)
+{
+if (b[i + j] >= 32 && b[i + j] <= 126)
+putchar(b[i + j]);
+else
+putchar('.');
+}
+}
+
+putchar('\n');
+}
+}
+}
