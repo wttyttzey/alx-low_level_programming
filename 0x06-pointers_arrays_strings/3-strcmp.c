6751023ea0fdf80704951f71fd3109b1a898b953
wttyttzey <alekikimii@gmail.com>
2023-07-27 00:14:52 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,10 +4,10 @@
  * _strcmp - Compares two strings.
  * @s1: The first string.
  * @s2: The second string.
- *
- * Return: 0 if the strings are equal, negative if s1 is less than s2,
- *         and positive if s1 is greater than s2.
+ * Return: 0 if the strings are equal, negative if s1 is les than s2,
+ * and positive if s1 is greater than s2.
  */
+
 int _strcmp(char *s1, char *s2)
 {
 int i = 0;
@@ -18,7 +18,6 @@ if (s1[i] == ('\0')
 return 0;
 i++;
 }
-
 return (s1[i] - s2[i]);
 }
 
