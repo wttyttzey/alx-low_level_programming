9afbb630d78be8498abe4f10f6e64a5db5ee5f15
wttyttzey <alekikimii@gmail.com>
2023-07-28 06:53:57 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,36 @@
 #include "main.h"
 
 /**
- * print_number - print an integer
- * @n: the integer to be printed.
- *
- */
-
+* print_number - prints an integer
+* @n: the integer to print
+*/
 void print_number(int n)
 {
+int i, j, k, d;
 
-unsigned int num = n;
-
+/ if n is negative, print a minus sign and make it positive /
 if (n < 0)
 {
-putchar('-');
-num = -num;
+_putchar('-');
+n = -n;
 }
 
-if ((num / 10) > 0)
-print_number(num / 10);
-putchar((num % 10) + '0');
+/ find the number of digits in n /
+for (i = 0, j = n; j > 0; i++)
+j /= 10;
+
+/ if n is zero, print zero /
+if (i == 0)
+_putchar('0');
+
+/ loop through the digits of n from left to right /
+for (j = i - 1; j >= 0; j--)
+{
+/ find the power of 10 for the current position /
+for (k = 1, d = 0; d < j; d++)
+k *= 10;
+
+/ print the digit at the current position /
+_putchar((n / k) % 10 + '0');
 }
-
-
+}
