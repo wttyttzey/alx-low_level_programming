37f04afbd449da611a8bd840ab42923baf38eb59
wttyttzey <alekikimii@gmail.com>
2023-07-28 08:43:27 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,7 +14,7 @@ n1 = (n < 0) ? -n : n;
 
 if (n < 0)
 {
-_putchar('-');
+putchar('-');
 }
 
 if (n1 / 10 != 0)
@@ -22,5 +22,5 @@ if (n1 / 10 != 0)
 print_number(n1 / 10);
 }
 
-_putchar(n1 % 10 + '0');
+putchar(n1 % 10 + '0');
 }
