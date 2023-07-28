6b69d3f325d2cd3b7e2d43b7cfd0299a078074e3
wttyttzey <alekikimii@gmail.com>
2023-07-28 07:11:55 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,36 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
-* print_number - prints an integer
-* @n: the integer to print
-*/
-void print_number(int n)
+int main(void)
 {
-int i, j, k, d;
+  int n;
+  int a[5];
+  int *p;
 
-/ if n is negative, print a minus sign and make it positive /
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-
-/ find the number of digits in n /
-for (i = 0, j = n; j > 0; i++)
-j /= 10;
-
-/ if n is zero, print zero /
-if (i == 0)
-_putchar('0');
-
-/ loop through the digits of n from left to right /
-for (j = i - 1; j >= 0; j--)
-{
-/ find the power of 10 for the current position /
-for (k = 1, d = 0; d < j; d++)
-k *= 10;
-
-/ print the digit at the current position /
-_putchar((n / k) % 10 + '0');
-}
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
