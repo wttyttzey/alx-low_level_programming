b77e74b7d75c0c0ba16655e6d6fbe15661c40412
wttyttzey <alekikimii@gmail.com>
2023-07-28 00:55:58 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * leet - Encodes into 1337
- * @n: The input value
+ * leet - Encodes a string into leet speak (1337 speak).
+ * @n: The input string to be encoded.
  *
- * Return: n value
+ * Return: A pointer to the modified string.
  */
 char *leet(char *n)
 {
@@ -12,16 +12,18 @@ int i, j;
 char s1[] = "aAeEoOtTlL";
 char s2[] = "4433007711";
 
-for (i = 0; n[i] != '\0';  i++)
+for (i = 0; n[i] != '\0'; i++)
 {
 for (j = 0; j < 10; j++)
 {
 if (n[i] == s1[j])
 {
-(n[i] == s1[j])
+n[i] = s2[j]; /* Replace the character with its leet speak equivalent */
+break; /* Stop searching for a match once we found one */
 }
 }
 }
 
 return (n);
 }
+
