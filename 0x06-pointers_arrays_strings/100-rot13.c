e7b2555e23662249a2f4ca3690cd1abb44eb14d5
wttyttzey <alekikimii@gmail.com>
2023-07-28 02:24:01 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,29 +13,24 @@ char *rot13_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char *rot13_codes = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 if (str == NULL)
-return (NULL);
+return NULL;
 
 while (*ptr)
 {
 int i;
-int found = 0;
 
 for (i = 0; rot13_chars[i]; i++)
 {
 if (*ptr == rot13_chars[i])
 {
 *ptr = rot13_codes[i]; /* Replace the character with its rot13 equivalent */
-found = 1;
 break;
 }
 }
 
-if (!found)
-ptr++; // If the character is not in the rot13_chars, move to the next character.
-
 ptr++;
 }
 
-return (str);
+return str;
 }
 
