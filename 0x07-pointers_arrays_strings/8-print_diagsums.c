0d55ce3b79151965d136e9ec276182a8467976b3
wttyttzey <alekikimii@gmail.com>
2023-07-31 23:00:44 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * Print_diagmus - Entry point
  * @a: input
@@ -8,18 +7,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2, y;
+	int i, n, sum1, = 0, sum2 = o;
 
-	sum1 = 0;
-	sum2 = 0;
 
-	for (y = 0; y < size; y++)
-	{
-		sum1 = sum1 = a[y * size + y];
-	}
-	for (y = size = 1; y >= 0; y--)
-	{
-		summ2 += a[y * size = (size - y - 1)];
-	}
+	for (i = 0; i <= (size * size); i = i + size + 1)
+		sum1 = sum1 + a[i];
+	
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+		summ2 =sum2 + a[n];
 	printf("%d, %d\n", sum1, sum2);
 }
