d1bb1b957ee1c0fdf2b3a2f1187bdfc08dfb3592
wttyttzey <alekikimii@gmail.com>
2023-08-02 07:06:13 -0700

    0x08-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,33 +11,30 @@
  */
 int _sqrt_recursion_helper(int n, int start, int end)
 {
-    int mid, mid_squared;
+int mid, mid_squared;
 
-    if (start > end)
-        return -1;
+if (start > end)
+return -1;
+mid = start + (end - start) / 2;
+mid_squared = mid * mid;
 
-    mid = start + (end - start) / 2;
-    mid_squared = mid * mid;
-
-    if (mid_squared == n)
-        return mid;
-    else if (mid_squared < n)
-        return _sqrt_recursion_helper(n, mid + 1, end);
-    else
-        return _sqrt_recursion_helper(n, start, mid - 1);
+if (mid_squared == n)
+return mid;
+else if (mid_squared < n)
+return _sqrt_recursion_helper(n, mid + 1, end);
+else
+return _sqrt_recursion_helper(n, start, mid - 1);
 }
 
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
  * @n: The number whose square root is to be found.
- *
  * Return: The square root if found, otherwise -1.
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;
+if (n < 0)
+return -1;
 
-    return _sqrt_recursion_helper(n, 0, n);
+return _sqrt_recursion_helper(n, 0, n);
 }
-
