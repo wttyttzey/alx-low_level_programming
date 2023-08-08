09b9973baccdef735328cbc5350305fff23eb7e6
wttyttzey <alekikimii@gmail.com>
2023-08-07 22:11:50 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grind(int width, int height);
+void free_grind(int **grind, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif /* MAIN_H */
