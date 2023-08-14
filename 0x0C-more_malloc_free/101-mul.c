a1bf6e40bfef3415a60c9ff3fe5804968beafc9d
wttyttzey <alekikimii@gmail.com>
2023-08-14 01:23:07 -0700

    malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,7 +8,7 @@
  */
 int _isdigit(char c)
 {
-    return ((c >= '0' && c <= '9'));
+return ((c >= '0' && c <= '9'));
 }
 
 /**
@@ -18,20 +18,20 @@ int _isdigit(char c)
  */
 int _atoi(char *s)
 {
-    int result = 0;
+int result = 0;
 
-    while (*s)
-    {
-        if (!_isdigit(*s))
-        {
-            write(1, "Error\n", 6);
-            exit(98);
-        }
+while (*s)
+{
+if (!_isdigit(*s))
+{
+write(1, "Error\n", 6);
+exit(98);
+}
 
-        result = result * 10 + (*s - '0');
-        s++;
-    }
-    return( result);
+result = result * 10 + (*s - '0');
+s++;
+}
+return (result);
 }
 
 /**
@@ -42,7 +42,7 @@ int _atoi(char *s)
  */
 int _mul(int num1, int num2)
 {
-    return( num1 * num2);
+return (num1 * num2);
 }
 
 /**
@@ -53,46 +53,45 @@ int _mul(int num1, int num2)
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
+int num1, num2, result;
 
-    if (argc != 3)
-    {
-        write(1, "Error\n", 6);
-        exit(98);
-    }
+if (argc != 3)
+{
+write(1, "Error\n", 6);
+exit(98);
+}
 
-    if (!_isdigit(*argv[1]) || !_isdigit(*argv[2]))
-    {
-        write(1, "Error\n", 6);
-        exit(98);
-    }
+if (!_isdigit(*argv[1]) || !_isdigit(*argv[2]))
+{
+write(1, "Error\n", 6);
+exit(98);
+}
 
-    num1 = _atoi(argv[1]);
-    num2 = _atoi(argv[2]);
-    result = _mul(num1, num2);
+num1 = _atoi(argv[1]);
+num2 = _atoi(argv[2]);
+result = _mul(num1, num2);
 
-    if (result == 0)
-    {
-        write(1, "0\n", 2);
-    }
-    else
-    {
-        char buffer[20];
-        int i = 0;
+if (result == 0)
+{
+write(1, "0\n", 2);
+}
+else
+{
+char buffer[20];
+int i = 0;
 
-        while (result > 0)
-        {
-            buffer[i++] = result % 10 + '0';
-            result /= 10;
-        }
+while (result > 0)
+{
+buffer[i++] = result % 10 + '0';
+result /= 10;
+}
 
-        while (i > 0)
-        {
-            write(1, &buffer[--i], 1);
-        }
-        write(1, "\n", 1);
-    }
-
-    return (0);
+while (i > 0)
+{
+write(1, &buffer[--i], 1);
+}
+write(1, "\n", 1);
+}
+return (0);
 }
 
